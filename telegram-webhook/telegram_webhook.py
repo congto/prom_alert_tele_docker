1031b99c8731ed905c6c032c86aabab95a81a1f6
Tô Thành Công <tcvn1985@gmail.com>
2025-11-23 15:21:56 +0700

    Update telegram_webhook.py

--- telegram-webhook/telegram_webhook.py
+++ telegram-webhook/telegram_webhook.py
@@ -32,9 +32,9 @@ def alert():
     print("Received alert:", data)
     for alert in data.get("alerts", []):
         msg = f"<b>{alert['status'].upper()}</b>: {alert['labels'].get('alertname')}\n"
-        msg += f"<b>Уровень:</b> {alert['labels'].get('severity', 'N/A')}\n"
-        msg += f"<b>Сводка:</b> {alert['annotations'].get('summary', '')}\n"
-        msg += f"<b>Описание:</b> {alert['annotations'].get('description', '')}"
+        msg += f"<b>Severity:</b> {alert['labels'].get('severity', 'N/A')}\n"
+        msg += f"<b>Summary:</b> {alert['annotations'].get('summary', '')}\n"
+        msg += f"<b>Description:</b> {alert['annotations'].get('description', '')}"
         print(f"[DEBUG] Constructed message:\n{msg}")
         send_telegram(msg)
     return "ok", 200
